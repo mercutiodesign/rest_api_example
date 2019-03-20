5dd42789fb531cba7a73b451f3ba71b4afe7beb4
Martin Dreher <luma.inc@gmail.com>
2019-03-20 20:31:16 +0100

    further view improvements

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('post/<int:post_id>', views.post, name='post'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views import generic
 from .models import Post
 
 
-def index(request):
-    return render(request, 'news/index.html', {'posts': Post.objects.all()})
+class IndexView(generic.ListView):
+    template_name = 'news/index.html'
+    context_object_name = 'posts'
 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Post.objects.order_by('-date')
 
 def post(_request, post_id):
     return HttpResponse("post %d" % post_id)
