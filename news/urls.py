2b818eb2ffae8efb50e33f08431b6164320bc404
Martin Dreher <luma.inc@gmail.com>
2019-03-21 00:09:43 +0100

    add django favicon

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='index'),
+    path('favicon.ico', RedirectView.as_view(url='/static/news/favicon.ico', permanent=True)),
     path('new-post/', views.PostCreate.as_view(), name='new-post'),
     path('delete-post/<int:pk>', views.PostDelete.as_view(), name='delete-post'),
     path('post/<int:pk>', views.PostDetail.as_view(), name='post'),
