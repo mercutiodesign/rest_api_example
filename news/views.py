616d6c0220ed53d0261159c73970c513d54553de
Martin Dreher <luma.inc@gmail.com>
2019-03-20 16:51:41 +0100

    implemented basic overview

--- news/views.py
+++ news/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
 
-def index(_request):
-    return HttpResponse("Hello, world. You're at the news index.")
+
+def index(request):
+    return render(request, 'news/index.html', {'posts': Post.objects.all()})
+
+
+def post(_request, post_id):
+    return HttpResponse("post %d" % post_id)
