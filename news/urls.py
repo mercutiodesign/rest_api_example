92700cd8915d555701a796dc88b62b0e947d9765
Martin Dreher <luma.inc@gmail.com>
2019-03-21 13:14:18 +0100

    fix indentation

--- news/urls.py
+++ news/urls.py
@@ -7,7 +7,7 @@ from .rest_api import api_urls
 urlpatterns = [
     path('', views.PostList.as_view(), name='index'),
     path('favicon.ico', RedirectView.as_view(url='/static/news/favicon.ico', permanent=True)),
-                  path('new-post', views.PostCreate.as_view(), name='new-post'),
+    path('new-post', views.PostCreate.as_view(), name='new-post'),
     path('delete-post/<int:pk>', views.PostDelete.as_view(), name='delete-post'),
     path('post/<int:pk>', views.PostDetail.as_view(), name='post'),
-              ] + api_urls
+] + api_urls
